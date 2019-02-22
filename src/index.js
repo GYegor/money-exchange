3b6903e966822e876d3b3e9898a6091453e19f54
GYegor <yahor2011@gmail.com>
2019-02-23 00:59:42 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+module.exports = function makeExchange(currency){
+    var purse = {};
+  if (currency == 0) return purse;
+  else if (currency > 10000) {
+  purse.error = "You are rich, my friend! We don't have so much coins for exchange";
+  return purse;} 
+  else if (currency /50 >= 1) {
+    purse.H = ~~(currency/50);
+    currency = currency % 50;}
+    if (currency/25 >= 1) {
+    purse.Q = ~~(currency/25);
+    currency = currency % 25;} 
+    if (currency/10 >= 1) {
+    purse.D = ~~(currency/10);
+    currency = currency % 10;} 
+    if (currency/5 >= 1) {
+    purse.N = ~~(currency/5);
+    currency = currency % 5;}
+    if (currency >= 1) {
+    purse.P = currency;
+    }
+    return purse;
+  };
+  
